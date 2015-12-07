cf7b319df12132ce53adcd7f5f1eaae593020bb5
jimflim <yann@jimflim.com>
2015-12-07 11:22:25 +0100

    added Trim tool, both tools now under Curve CAD Tools Tab

--- extend.py
+++ extend.py
@@ -1,13 +1,31 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
 bl_info = {
     "name": "Curve Extend",
     "description": "Extend selected endpoint(s) for Bezier Splines",
     "author": "jimflim",
     "version": (0, 1),
     "blender": (2, 75, 0),
-    "category": "Curve Tools",
+    "category": "Add Curve",
     "location": "View3D > Tools > Curves",
     "warning": "",
-    "wiki_url": ""
+    "wiki_url": "https://github.com/jimflim/blender-scripts"
     }
 
 
@@ -228,7 +246,8 @@ class ExtendTool(bpy.types.Operator):
 class ExtendToolPanel(bpy.types.Panel):
     bl_label = "Extend Tool"
     bl_space_type = 'VIEW_3D'
-    bl_region_type = 'TOOL_PROPS'
+    bl_region_type = 'TOOLS'
+    bl_category = 'Curve CAD Tools'
 
     def draw(self, context):
         layout = self.layout
